2c7cf4f378c14c65e906b7e97a9faf8516d8cf84
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-11-24 17:12:40 -0600

    Add rewrite proxy support for lomiri-push-service
    This should help add a UP compatibility layer to the default push server
    used by UBPorts/Ubuntu Touch.

---
+++ rewrite/lomiri.go
@@ -0,0 +1,72 @@
+package rewrite
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/karmanyaahm/up_rewrite/utils"
+)
+
+type Lomiri struct {
+	Enabled bool `env:"UP_REWRITE_LOMIRI_ENABLE"`
+	APIURL  string
+}
+
+func (l Lomiri) Path() string {
+	if l.Enabled {
+		return "/lomiri"
+	}
+	return ""
+}
+
+type lomiriSend struct {
+	Token    string `json:"token"`
+	AppId    string `json:"appid"`
+	ExpireOn string `json:"expire_on,omitempty"`
+	Data     string `json:"data"`
+	// clear all pending messages for appid
+	ClearPending bool `json:"clear_pending,omitempty"`
+	// replace pending messages with the same replace_tag
+	ReplaceTag string `json:"replace_tag,omitempty"`
+}
+
+func (l Lomiri) Req(body []byte, req http.Request) (*http.Request, error) {
+	token := req.URL.Query().Get("token")
+	appid := req.URL.Query().Get("appid")
+
+	newBody, err := utils.EncodeJSON(lomiriSend{
+		Token: token,
+		AppId: appid,
+		Data:  string(body),
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	newReq, err := http.NewRequest(http.MethodPost, l.APIURL, newBody)
+	newReq.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return nil, err
+	}
+
+	return newReq, nil
+}
+
+func (l Lomiri) RespCode(resp *http.Response) int {
+	switch resp.StatusCode {
+	case 429:
+		return 429
+	default:
+		return 202
+	}
+}
+
+func (l *Lomiri) Defaults() (failed bool) {
+	if !l.Enabled {
+		return
+	}
+	l.APIURL = "https://push.ubports.com/notify"
+	return
+}
